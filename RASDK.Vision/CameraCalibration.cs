62a785583f5a91d6e3799bcf607fedce8f616a99
ziteh <honmonoh@gmail.com>
2022-07-25 15:09:05 +0800

    Add camera calibration class (#10

---
+++ RASDK.Vision/CameraCalibration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace RASDK.Vision
+{
+    public class CameraCalibration
+    {
+    }
+}
