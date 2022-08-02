61e7f62207092daee501bdbf92f1d99668bff18a
ziteh <honmonoh@gmail.com>
2022-08-02 19:33:34 +0800

    Add feature of AruCo

---
+++ RASDK.Vision/Aruco.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Aruco;
+
+namespace RASDK.Vision
+{
+    public class Aruco
+    {
+        public Dictionary Dictionary = new Dictionary(Dictionary.PredefinedDictionaryName.Dict4X4_50);
+
+        public int Detect(Image<Bgr, byte> image,
+                          out VectorOfVectorOfPointF corners,
+                          out VectorOfInt ids)
+        {
+            corners = new VectorOfVectorOfPointF();
+            ids = new VectorOfInt();
+            ArucoInvoke.DetectMarkers(image, Dictionary, corners, ids, DetectorParameters.GetDefault());
+            return (int)ids.Length;
+        }
+    }
+}
